134d72c453b3a8b6fd7428fdec5e49efb3806ed4
PC <PC@DESKTOP-HS6125F>
2022-12-20 10:13:42 +0300

    Activites implemented

--- app/src/main/java/net/cootz/activityswitch/MainActivity.java
+++ app/src/main/java/net/cootz/activityswitch/MainActivity.java
@@ -1,14 +1,55 @@
 package net.cootz.activityswitch;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.transfer_data_edit_text);
     }
+
+    public void onExitCLick(View view)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.drawable.warning_sign_red);
+        builder.setView(R.layout.dialog_exit);
+        builder.setPositiveButton(
+                R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                }
+        ).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void onDialogClick(View view)
+    {
+        Intent i = new Intent(this, two.class);
+        i.putExtra("etData", text.getText().toString());
+
+        startActivityForResult(i, 555);
+    }
+
+
 }
---
+++ app/src/main/java/net/cootz/activityswitch/two.java
@@ -0,0 +1,43 @@
+package net.cootz.activityswitch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.zip.Inflater;
+
+public class two extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+
+        Intent i = getIntent();
+        String transfered = i.getStringExtra("etData");
+
+        TextView text = findViewById(R.id.intent_text_view);
+        text.setText(transfered);
+    }
+
+    public void onOkClick(View view)
+    {
+        TextView text = findViewById(R.id.intent_text_view);
+
+        Intent i = new Intent();
+        i.putExtra("resData", text.getText());
+
+        setResult(RESULT_OK);
+        finish();
+    }
+
+    public void onCanceledClick(View view)
+    {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+
+}
