affb0425dae53d5a6b8e860202b78883f6426cbb
DoesTheThing <artiom.matveeff2014@ya.ru>
2022-12-20 18:21:59 +0300

    De

--- app/src/main/java/net/cootz/activityswitch/MainActivity.java
+++ app/src/main/java/net/cootz/activityswitch/MainActivity.java
@@ -7,12 +7,15 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText text;
+    CheckBox ch1;
+    CheckBox ch2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         text = findViewById(R.id.transfer_data_edit_text);
+        ch1 = findViewById(R.id.checkbox_one);
+        ch2 = findViewById(R.id.checkbox_two);
     }
 
     @Override
@@ -29,9 +34,11 @@ public class MainActivity extends AppCompatActivity {
             if (data != null)
             {
                 String res = data.getStringExtra("resData");
+                Boolean sw1 = data.getBooleanExtra("sw1", false);
+                Boolean sw2 = data.getBooleanExtra("sw2", false);
 
-
-
+                ch1.setChecked(sw1);
+                ch2.setChecked(sw2);
             }
         }
 
@@ -60,10 +67,36 @@ public class MainActivity extends AppCompatActivity {
         dialog.show();
     }
 
+    public void onEditDialogCLick(View view)
+    {
+        EditText editText = new EditText(this);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.drawable.warning_sign_red);
+        builder.setView(editText);
+        builder.setPositiveButton(
+                R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        text.setText(editText.getText());
+                    }
+                }
+        ).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
     public void onDialogClick(View view)
     {
         Intent i = new Intent(this, two.class);
         i.putExtra("etData", text.getText().toString());
+        i.putExtra("ch1", ch1.isChecked());
+        i.putExtra("ch2", ch2.isChecked());
 
         startActivityForResult(i, 555);
     }
--- app/src/main/java/net/cootz/activityswitch/two.java
+++ app/src/main/java/net/cootz/activityswitch/two.java
@@ -5,19 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.util.zip.Inflater;
 
 public class two extends AppCompatActivity {
+    private Switch sw1;
+    private Switch sw2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
 
+        sw1 = findViewById(R.id.switch_one);
+        sw2 = findViewById(R.id.switch_two);
+
         Intent i = getIntent();
         String transfered = i.getStringExtra("etData");
+        Boolean issw1 = i.getBooleanExtra("ch1", false);
+        Boolean issw2 = i.getBooleanExtra("ch2", false);
+
+        sw1.setChecked(issw1);
+        sw2.setChecked(issw2);
 
         TextView text = findViewById(R.id.intent_text_view);
         text.setText(transfered);
@@ -29,6 +40,8 @@ public class two extends AppCompatActivity {
 
         Intent i = new Intent();
         i.putExtra("resData", text.getText());
+        i.putExtra("sw1", sw1.isChecked());
+        i.putExtra("sw2", sw2.isChecked());
 
         setResult(RESULT_OK, i);
         finish();
